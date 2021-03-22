94f302239c0b2da9c182bb51c60bace1e5154dcd
JazzBoy69 <joel.ashford@gmail.com>
2021-03-22 07:04:16 -0500

    restored ctrl and shift on left home row

--- keyboards/ergodox_ez/keymaps/codemax/keymap.c
+++ keyboards/ergodox_ez/keymaps/codemax/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // left hand ---------------------------------------------------------------------------------------------------
     KC_ESCAPE,          KC_F1,        KC_F2,           KC_F3,        KC_F4,        KC_F5,          TG(4),  
     KC_GRAVE,           KC_Q,         KC_W,            KC_F,         KC_P,         KC_G,           TG(3),  
-    OSL(6),           LT(1,KC_A),  LALT_T(KC_R),       KC_S,         KC_T,       LT(2,KC_D), 
+    OSL(6),           LT(1,KC_A),  LALT_T(KC_R),   LCTL_T(KC_S),  LSFT_T(KC_T),       LT(2,KC_D), 
     OSM(MOD_LSFT),      KC_Z,         KC_X,            KC_C,         KC_V,         KC_B,           KC_F15,  
     KC_DELETE,      KC_APPLICATION, KC_MS_BTN2,     KC_ESCAPE,     KC_SPACE,                      
  // left thumb --------------------------------------------------------------------------------------------------
