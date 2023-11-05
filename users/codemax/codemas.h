bb5ace25343644272c201944d76d1c6a323f8a5f
Joel Ashford <joel.ashford@gmail.com>
2023-11-04 19:21:06 -0500

    caps and num lock on thumb pad

--- users/codemax/codemas.h
+++ users/codemax/codemas.h
@@ -168,8 +168,8 @@ enum custom_keycodes {
 #define TH_L2                                             KC_ESCAPE
 #define TH_L3                                             LALT(KC_HOME)
 #define THUMB_R1                                          SC_SUPERQUES,    LCTL_T(KC_SPACE)
-#define TH_R2                                             KC_MS_BTN4
-#define TH_R3                                             KC_MS_BTN5
+#define TH_R2                                             KC_CAPSLOCK
+#define TH_R3                                             TO(NUMPAD)
 
 
 #define ________________NUMPAD_L1_________________        TO(COLEMAK),    ___, ES_AMPR,   ES_PIPE,      ___,  ___
